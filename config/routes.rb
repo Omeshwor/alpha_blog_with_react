bafeeaba13f50bb721e6f2663c7829fae0851fca
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-12-10 11:18:13 -0600

    Add login page and login/logout actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
     resources :articles
     resources :users, except: [:new]
+
     root "articles#index"
-    get 'signup', to: "users#new"
+
+    get     'signup',   to: "users#new"
+
+    get     'login',    to: "sessions#new"
+    post    'login',    to: "sessions#create"
+    delete  'logout',   to: "sessions#destroy"
 end

47aed29f283689d4d85639c9a976b72791d1417f
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-12-10 12:54:56 -0600

    Add signup feature to sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to the blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -25,7 +26,6 @@ class UsersController < ApplicationController
     end
 
     def update
-
         if @user.update(user_params)
             flash[:success] = "User successfully updated"
             redirect_to articles_path

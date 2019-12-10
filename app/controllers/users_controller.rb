578c13e086605595185d21996a0488551dda7017
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-12-10 12:47:25 -0600

    Add actions restriction from controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -44,6 +45,13 @@ class UsersController < ApplicationController
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:danger] = "You do not have access for this action"
+            redirect_to root_path
+        end
     end
 
 end

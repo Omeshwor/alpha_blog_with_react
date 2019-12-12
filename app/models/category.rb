9d86cf02dd444d73305b179f44fcf5934bfd95e3
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-12-12 14:02:25 -0600

    Add Article Category model and relevant associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+    has_many :article_categories
+    has_many :articles, through: :article_categories
     before_save {self.name = name.capitalize }
     validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
 

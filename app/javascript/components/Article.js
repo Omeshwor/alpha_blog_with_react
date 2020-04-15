49fcb0b5b55360f1744835d9b5052df8edebec8e
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2020-04-14 22:43:40 -0500

    Move article body to react component

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,23 +1,32 @@
 import React from "react"
 import PropTypes from "prop-types"
 class Article extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <div className="article-title">
-          <a href={this.props.path}>{this.props.title}</a>
-        </div>
-        <div className="article-body">
-          {this.props.description}
-        </div>
-      </React.Fragment>
-    );
-  }
+	render () {
+		return (
+			<React.Fragment>
+				<div className='article-title'>
+   					<a href={this.props.path}>{this.props.title}</a>
+ 				</div>
+ 				<div className='article-body'>
+   					{this.props.description}
+   					<div className="article-meta-details">
+       					<small>Created by: <em>{this.props.author.username}</em>,
+              				{this.props.created_at} ago , 
+              				last updated: {this.props.updated_at} ago
+       					</small>
+   					</div>
+ 				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 Article.propTypes = {
-  title: PropTypes.string,
-  path: PropTypes.string,
-  description: PropTypes.string
-};
+	title: PropTypes.string,
+	path:  PropTypes.string,
+	description: PropTypes.string,
+	author: PropTypes.string,
+	created_at: PropTypes.string,
+	updated_at: PropTypes.string
+  };
 export default Article
